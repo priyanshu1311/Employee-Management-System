2488fcd9116874017744a47d05482bb023b9cd63
Priyanshu Bandyopadhyay <bandyopadhyaypriyanshu@gmail.com>
2022-05-25 16:29:46 +0530

    Add files via upload

---
+++ Front-end/Angular_Front-end/src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { CreateEmployeeComponent } from './create-employee/create-employee.component';
+import { Employee } from './employee';
+import { UpdateEmployeeComponent } from './update-employee/update-employee.component';
+import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
+
+const routes: Routes = [
+  { path: 'employees', component: EmployeeListComponent },
+  { path: 'create-employee', component: CreateEmployeeComponent },
+  { path: 'employee-details/:id', component: EmployeeDetailsComponent },
+  { path: 'update-employee/:id', component: UpdateEmployeeComponent },
+  { path: '', redirectTo: 'employees', pathMatch: 'full' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
